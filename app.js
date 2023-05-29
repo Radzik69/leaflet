5f77aff2d9d7aed47850376b557fd6d64b7dfd56
Radzik69 <126457866+Radzik69@users.noreply.github.com>
2023-05-29 11:21:47 +0200

    z lekcji 2
    w domu zrobic ze trzeba wpisac w input nazwe wojewodztwa itp

--- app.js
+++ app.js
@@ -22,9 +22,10 @@ function addMarker(e) {
         [e.latlng.lat, e.latlng.lng]
     ];
 
-    
+
     line = L.polyline(tab).addTo(map);
     updateDistance(); // Aktualizuj wyświetlany dystans
+    console.log(marker)
 }
 
 var lat1 = 52.18669636428679;
@@ -65,6 +66,63 @@ function updateDistance() {
     }
 }
 
-for(let i=0;i<=woje.features.length-1;i++){
-    L.geoJSON(woje.features[i]).addTo(map)
+var nazwyArray = [];
+
+for (let i = 0; i <= woje.features.length - 1; i++) {
+    var wojew = L.geoJSON(woje.features[i], { color: "blue" }).addTo(map)
+    nazwyArray.push(woje.features[i].properties.nazwa);
+    wojew.on("click", showName);
+    // wojew.on("mouseover", setColor);
+    // wojew.on("mouseout", setColorPrew);
+}
+
+var poprzednieWojewodztwo = null;
+
+function showName(e) {
+    console.log(e.layer.feature.properties.nazwa);
+}
+
+// function setColor(e) {
+//     this.setStyle({
+//         color: "green"
+//     });
+// }
+
+// function setColorPrew() {
+//     this.setStyle({
+//         color: "blue"
+//     });
+// }
+
+function randomWoje(min, max) {
+    console.log(nazwyArray);
+    if (nazwyArray.length >= 1) { 
+        if (poprzednieWojewodztwo) {
+            map.removeLayer(poprzednieWojewodztwo);
+            
+        }
+        var index = Math.floor(Math.random() * nazwyArray.length)
+        var item = nazwyArray[index]
+        nazwyArray.splice(index, 1)
+        document.getElementById("wylosowanie").innerHTML = item
+        
+        for (let i = 0; i < woje.features.length; i++) {
+            if (woje.features[i].properties.nazwa === item) {
+                poprzednieWojewodztwo = L.geoJSON(woje.features[i], { color: "red" }).addTo(map);
+                
+            }
+        }
+    } 
+    
+    else {
+        document.getElementById("wylosowanie").innerHTML = "wylosowano wszystkie"
+        poprzednieWojewodztwo.setStyle({ color: "blue" });
+    }
+}
+
+
+
+function nazywanie(){
+    const input = document.getElementById("text")
+    input.setAttribute("placeholder","wpisz nazwe podswietlonego wojewodztwa")
 }
